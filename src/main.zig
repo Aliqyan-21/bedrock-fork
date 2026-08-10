17532538416d33f934525afa94bb7c4972b06d36
Aliqyan-21 <aliqyanabid21@gmail.com>
2026-08-10 19:50:06 +0530

    Revert "."
    This reverts commit c9bf875dc825cbc09212a4dfa46d3da5c3f914a3.

--- src/main.zig
+++ src/main.zig
@@ -12,7 +12,7 @@ pub fn main() !void {
 
     const source =
         \\import io
-        \\func add(x i32, y: i32) -> i32
+        \\func proc add(x i32, y: i32) -> i32
         \\  return x + y;
         \\end
         \\
