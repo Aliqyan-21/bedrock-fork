1b52e9d478a28732b7d88f7c249357021d05dc3b
Aliqyan-21 <aliqyanabid21@gmail.com>
2026-08-21 14:47:40 +0530

    patch(parser): attempt to handle erors in parse_call_expr
    - mostly nicely

--- src/parser.zig
+++ src/parser.zig
@@ -919,18 +919,34 @@ pub const Parser = struct {
             return c_expr;
         }
         while (true) {
-            if (tok.type == .r_paren or tok.type == .eof) break;
-            // TODO: check for comma missing error
-            if (tok.type == .comma) {
-                _ = try self.lexer.next();
-            }
             const arg = try self.parse_call_arg();
             try c_expr.args.append(self.allocator, arg);
-            tok = try self.lexer.peek_token();
+
+            tok = try self.lexer.next();
+            switch (tok.type) {
+                .r_paren => break,
+                .comma => {
+                    const nxt = try self.lexer.peek_token();
+                    if (nxt.type == .r_paren) {
+                        _ = try self.lexer.next();
+                        break;
+                    }
+                    continue;
+                },
+                else => {
+                    try self.compiler.addError("expected ',' or ')'", err.Severity.Error, tok);
+                    try self.sync(&.{ .comma, .r_paren });
+                    const hmm = try self.lexer.peek_token();
+                    if (hmm.type == .comma) {
+                        _ = try self.lexer.next();
+                        continue;
+                    } else if (hmm.type == .r_paren) {
+                        _ = try self.lexer.next();
+                        break;
+                    } else break;
+                },
+            }
         }
-
-        if (tok.type == token.TokenType.r_paren) _ = try self.lexer.next();
-
         return c_expr;
     }
 
