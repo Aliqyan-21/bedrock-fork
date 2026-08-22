f89ce9d229c162f8ce8c78e0e61646e4b345f185
Aliqyan-21 <aliqyanabid21@gmail.com>
2026-08-22 22:21:13 +0530

    feat(scope): smart scoping

--- src/sema/scope.zig
+++ src/sema/scope.zig
@@ -16,10 +16,22 @@ pub const Symbol = struct {
 };
 
 pub const Scope = struct {
+    id: Id,
+    parent: ?*Scope,
     symbols: std.StringHashMap(Symbol), // for O(1)
 
-    pub fn init(allocator: std.mem.Allocator) Scope {
+    pub const Id = enum {
+        root, // root (var_def, const_def)
+        block, // if, elif, else, etc...
+        func, // both func and proc
+        loop,
+        unsafe,
+    };
+
+    pub fn init(allocator: std.mem.Allocator, id: Id, parent: ?*Scope) Scope {
         return .{
+            .id = id,
+            .parent = parent,
             .symbols = std.StringHashMap(Symbol).init(allocator),
         };
     }
@@ -32,5 +44,25 @@ pub const Scope = struct {
     pub fn declare(self: *Scope, symbol: Symbol) !void {
         if (self.symbols.contains(symbol.name)) return error.DuplicateName;
         try self.symbols.put(symbol.name, symbol);
+    }
+
+    // resolve name keep going outward towards upper scope
+    pub fn resolve(self: *Scope, name: []const u8) ?Symbol {
+        var cur: ?*Scope = self;
+        while (cur) |s| {
+            if (s.symbols.get(name)) |sym| return sym;
+            cur = s.parent;
+        }
+        return null;
+    }
+
+    // search outward for a given scope id
+    pub fn enclosing(self: *Scope, id: Id) ?Symbol {
+        var cur: ?*Scope = self;
+        while (cur) |s| {
+            if (s.id == id) return s;
+            cur = s.parent;
+        }
+        return null;
     }
 };
