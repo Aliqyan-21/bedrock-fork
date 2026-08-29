46737344eb5b05bd6fff7d6d66e405d84efbea56
Aliqyan-21 <aliqyanabid21@gmail.com>
2026-08-29 14:40:50 +0530

    feat(typesystem): binary -> logical op check

--- src/compiler.zig
+++ src/compiler.zig
@@ -83,20 +83,20 @@ pub const Compiler = struct {
         self.sema.deinit();
         try self.emitErrors();
 
-        var c = codegen.Codegen.init(self.allocator, self);
-        self.mod = try c.codegen();
-        self.ctx = c.ctx;
-        var error_message: [*c]u8 = null;
-        const res = llvm.LLVMPrintModuleToFile(self.mod, "./corpus/codegen/dump.ll", &error_message);
-        if (res != 0) {
-            if (error_message) |msg| {
-                std.debug.print("LLVM: {s}\n", .{std.mem.span(msg)});
-                llvm.LLVMDisposeMessage(msg);
-            }
-        }
-
-        try self.jit();
-        c.deinit();
+        // var c = codegen.Codegen.init(self.allocator, self);
+        // self.mod = try c.codegen();
+        // self.ctx = c.ctx;
+        // var error_message: [*c]u8 = null;
+        // const res = llvm.LLVMPrintModuleToFile(self.mod, "./corpus/codegen/dump.ll", &error_message);
+        // if (res != 0) {
+        //     if (error_message) |msg| {
+        //         std.debug.print("LLVM: {s}\n", .{std.mem.span(msg)});
+        //         llvm.LLVMDisposeMessage(msg);
+        //     }
+        // }
+        //
+        // try self.jit();
+        // c.deinit();
         self.ast.deinit(self.allocator);
     }
 
