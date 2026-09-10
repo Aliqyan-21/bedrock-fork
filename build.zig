c423a9e0f8e05be0389e6ecd80f5f50b7f1eca51
vasu <vasucp1207@gmail.com>
2026-09-10 18:23:29 +0530

    jit memory.o link

--- build.zig
+++ build.zig
@@ -47,6 +47,18 @@ pub fn build(b: *std.Build) void {
         .root_module = exe.root_module,
     });
 
+    const memory = b.addObject(.{
+        .name = "memory",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/memory/memory.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    const copy_memory = b.addInstallFile(memory.getEmittedBin(), "memory.o");
+    b.getInstallStep().dependOn(&copy_memory.step);
+
     const run_exe_tests = b.addRunArtifact(exe_tests);
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_exe_tests.step);
